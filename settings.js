7c479bc20b8e6e717ec0f4bccd4c6e57bb53f65e
Kiefer Lam <mangopearandapples@gmail.com>
2017-03-21 02:03:50 +0000

    Added settings navigation panel functionality.

--- settings.js
+++ settings.js
@@ -8,29 +8,67 @@ const $ = require('jquery');
 
 var settings;
 
+var setting_panels = ['message_previews-li', 'application-li', 'system-li'];
+var selected_panel = setting_panels[0];
+
 $(document).ready(() => {
     settings = remote.getGlobal('settings');
     setup();
 });
 
-function validate(number){
+function validate(number) {
     return !isNaN(number);
 }
 
-function setup(){
+function setPanelListItemStyle() {
+    setting_panels.forEach((panel_name) => {
+        $('#' + panel_name).removeClass('settings_list_item_selected');
+    });
+    $('#' + selected_panel).addClass('settings_list_item_selected');
+}
+
+function setDisplaySettingsPanel(){
+    var panelID = $('#'+selected_panel).attr('data-panel');
+    //hide all panels
+    setting_panels.forEach((panel_name)=>{
+        var pan_id = $('#'+panel_name).attr('data-panel');
+        $('#'+pan_id).css({display: 'none'});
+    });
+    //Show one panel
+    $('#'+panelID).css({display: 'unset'});
+}
+
+function setup() {
+    setPanelListItemStyle();
+    setDisplaySettingsPanel();
+    setting_panels.forEach((panel_name) => {
+        $('#' + panel_name).click((event) => {
+            selected_panel = panel_name;
+            setPanelListItemStyle();
+            setDisplaySettingsPanel();
+        });
+    });
+
+    setupMessagePreviewPanel();
+    setupSystemPanel();
+}
+
+function setupMessagePreviewPanel() {
     $('#message_display_period-number').val(settings.message_display_period);
-    $('#message_display_period-number').on('mouseup keyup', (event)=>{
+    $('#message_display_period-number').on('mouseup keyup', (event) => {
         var value = $('#message_display_period-number').val();
-        if(validate(value)){
+        if (validate(value)) {
             $('#message_display_period-number').removeClass('error_outline');
             settings.message_display_period = value;
-        }else{
+        } else {
             $('#message_display_period-number').addClass('error_outline');
         }
     });
 }
 
+function setupSystemPanel() {
 
+}
 
 
 function mainLog(log) {
