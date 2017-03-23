c83ea029117c445bbc2f40383525c2e316e1a995
Kiefer Lam <mangopearandapples@gmail.com>
2017-03-23 23:15:39 +0000

    Fixed the extra line break after sending a message (pressing enter)

--- incomingmessage.js
+++ incomingmessage.js
@@ -20,7 +20,7 @@ var userInfo;
 var lastSenderID = 0;
 var lastMessageTime = '0';
 
-window.onerror = function(error, url, line){
+window.onerror = function (error, url, line) {
     mainLog("Error at line " + line + ": " + error);
 };
 
@@ -39,24 +39,24 @@ ipc.once('initMessageDetails', (event, threadinfo, messageInfo, preloadedUserInf
     mainLog('Initial message update.');
     threadId = messageInfo.message.threadID;
     userInfo = preloadedUserInfo;
-    completeData = {threadInfo: threadinfo, userInfo: messageInfo, message: messageInfo.message};
-    if(!DEBUG_LOCAL_MODE) {
+    completeData = { threadInfo: threadinfo, userInfo: messageInfo, message: messageInfo.message };
+    if (!DEBUG_LOCAL_MODE) {
         event.sender.send('pollPreloadedThreads', messageInfo.message.threadID);
-        ipc.once('preloadedThreadInfo', (event, threadData)=>{
-            if(threadData == null){
-                threadData = {imageSrc: null};
+        ipc.once('preloadedThreadInfo', (event, threadData) => {
+            if (threadData == null) {
+                threadData = { imageSrc: null };
             }
             $('#thread_pic').attr('src', threadData.imageSrc == null ? messageInfo.data.thumbSrc : threadData.imageSrc);
             $('#thread_name').text(threadinfo.name);
             var sender_name = messageInfo.data.name + ((messageInfo.data.alternateName != undefined) ? ' (' + messageInfo.data.alternateName + ')' : '');
-            $('#sender_name').text(messageInfo.data.isGroup ? sender_name:'');
+            $('#sender_name').text(messageInfo.data.isGroup ? sender_name : '');
             appendMessage(messageInfo.message);
             event.sender.send('readyToDisplay', $('body')[0].scrollHeight);
         });
     } else {
         appendMessage(messageInfo.message);
         event.sender.send('readyToDisplay', $('body')[0].scrollHeight);
-    } 
+    }
 });
 
 ipc.on('anotherMessage', (event, userInfo) => {
@@ -68,106 +68,109 @@ ipc.on('anotherMessage', (event, userInfo) => {
 function appendMessage(message) {
     var date = new Date(Date.now());
     var acc_to_min = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
-    if(lastSenderID != message.senderID || lastMessageTime != acc_to_min){
+    if (lastSenderID != message.senderID || lastMessageTime != acc_to_min) {
         lastSenderID = message.senderID;
         var timestamp = timestamp_html(date);
         lastMessageTime = acc_to_min;
         $('#messages_container').append('<div class="message-container">' + sender_img_html(message) + timestamp + message_html(message) + '</div>');
-    }else{
+    } else {
         $('#messages_container').append('<div class="message-container">' + message_html(message) + '</div>');
     }
     $('#content-div').height($('body')[0].scrollHeight);
 }
 
-function sender_img_html(message){
+function sender_img_html(message) {
     return '';
 }
 
-function message_html(message){
+function message_html(message) {
     return '<p class="message-body">' + message.body + '</p>';
 }
 
-function sender_name(){
-    if(completeData.message.isGroup && completeData.threadInfo.nicknames != null){
+function sender_name() {
+    if (completeData.message.isGroup && completeData.threadInfo.nicknames != null) {
         var nick = completeData.threadInfo.nicknames[lastSenderID.toString()];
-        if(nick != null) return nick + ' (' + userInfo[lastSenderID].name + ') ';
+        if (nick != null) return nick + ' (' + userInfo[lastSenderID].name + ') ';
     }
     return userInfo[lastSenderID].name;
 }
 
-function timestamp_html(date){
+function timestamp_html(date) {
     return '<p class="timestamp">' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ' ' + sender_name() + '</p>';
 }
 
-function userTimestamp(date, timestampId){
-    return '<p id="'+timestampId+'" class="userTimestamp"> ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ' </p>';
+function userTimestamp(date, timestampId) {
+    return '<p id="' + timestampId + '" class="userTimestamp"> ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ' </p>';
 }
 
 $('#close_button').click(() => {
     ipc.send('message_close');
 });
 
-function userMessagePara(messageId, replyMsg){
-    return '<p id="'+messageId+'" class="user-message-body unsent"> ' + replyMsg + ' </p>';
+function userMessagePara(messageId, replyMsg) {
+    return '<p id="' + messageId + '" class="user-message-body unsent"> ' + replyMsg + ' </p>';
 }
 
-function appendUserMessage(messageId, replyMsg, timestampId){
+function appendUserMessage(messageId, replyMsg, timestampId) {
     var nowDate = new Date(Date.now());
     var timestamp = nowDate.getHours() + ':' + nowDate.getMinutes();
-    if(lastSenderID != 'user' || lastMessageTime != timestamp){
+    if (lastSenderID != 'user' || lastMessageTime != timestamp) {
         var msgHtml = '<div class="user-message-container">' + userTimestamp(nowDate, timestampId) + userMessagePara(messageId, replyMsg) + '</div>';
         lastSenderID = 'user';
         lastMessageTime = timestamp;
-    }else{
+    } else {
         var msgHtml = '<div class="user-message-container">' + userMessagePara(messageId, replyMsg) + '</div>';
     }
-    $('#messages_container').append(msgHtml);  
+    $('#messages_container').append(msgHtml);
     $('#content-div').height($('body')[0].scrollHeight);
 }
 
-function sendMsg(replyMsg){
-        var messageId = Math.floor(Math.random() * 100000);
-        var timestampId = Math.floor(Math.random() * 100000);
-        appendUserMessage(messageId, replyMsg, timestampId);
-        if(!DEBUG_LOCAL_MODE){
-            //Send via api
-            ipc.send('apiSend', {body: replyMsg, thread: threadId});
-            ipc.once('apiSendCallback', (event, err, msgInfo)=>{
-                $('#'+messageId).removeClass('unsent');
-                $('#'+timestampId).val(msgInfo.timestamp);
-                if(err){
-                    $('#'+messageId).addClass('sendError');
-                    mainLog(err.error);
-                }
-            });
-        }
+function sendMsg(replyMsg) {
+    var messageId = Math.floor(Math.random() * 100000);
+    var timestampId = Math.floor(Math.random() * 100000);
+    appendUserMessage(messageId, replyMsg, timestampId);
+    if (!DEBUG_LOCAL_MODE) {
+        //Send via api
+        ipc.send('apiSend', { body: replyMsg, thread: threadId });
+        ipc.once('apiSendCallback', (event, err, msgInfo) => {
+            $('#' + messageId).removeClass('unsent');
+            $('#' + timestampId).val(msgInfo.timestamp);
+            if (err) {
+                $('#' + messageId).addClass('sendError');
+                mainLog(err.error);
+            }
+        });
+    }
 }
 
-$('#reply_button').click(()=>{
+$('#reply_button').click(() => {
     $('#message-main').append('<div id="reply-div"> <textarea id="reply_message" res></textarea> <button id="reply_send"></button> </div>');
     $('#reply_button').hide();
     const minHeight = 32;
     const maxHeight = 96;
     var shiftDown = false;
-    function sendFunction(){
+    function sendFunction() {
         var replyMsg = $('#reply_message').val();
-        if(replyMsg.length <= 0) return;
+        if (replyMsg.length <= 0) return;
         sendMsg(replyMsg);
-        $('#reply_message').val('');
-        $('#reply_message').height(minHeight);
-        resize();
     }
-    $('#reply_message').on('keyup keydown', (event)=> {
+    $('#reply_message').on('keyup keydown', (event) => {
         shiftDown = event.shiftKey;
-        if(event.keyCode == 13 && event.type == 'keydown'){
-            sendFunction();
+        if (event.keyCode == 13 && !shiftDown) {
+            if (event.type == 'keydown') {
+                sendFunction();
+            } else {
+                $('#reply_message').val('');
+                $('#reply_message').height(minHeight);
+                resize();
+            }
         }
     });
-    $('#reply_message').on('input', (event)=>{
-        while($('#reply_message').height() == $('#reply_message')[0].scrollHeight && $('#reply_message').height() > minHeight){
+    $('#reply_message').on('input', (event) => {
+        while ($('#reply_message').height() == $('#reply_message')[0].scrollHeight && $('#reply_message').height() > minHeight) {
             $('#reply_message').height($('#reply_message').height() - 1);
         }
-        while($('#reply_message')[0].scrollHeight > $('#reply_message').height() && $('#reply_message').height() < maxHeight){
+        while ($('#reply_message')[0].scrollHeight > $('#reply_message').height() && $('#reply_message').height() < maxHeight) {
             $('#reply_message').height($('#reply_message').height() + 1);
         }
         resize();
@@ -176,7 +179,7 @@ $('#reply_button').click(()=>{
     resize();
 });
 
-function resize(){
+function resize() {
     $('#content-div').height($('body')[0].scrollHeight);
     ipc.send('resizeHeight', $('body')[0].scrollHeight);
 }
