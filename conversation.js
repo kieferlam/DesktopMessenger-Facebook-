2e83915f8b79b5875ec3a70ee327584fcca2510d
Kiefer Lam <mangopearandapples@gmail.com>
2017-03-30 22:30:35 +0100

    Added styling for user messages. Added general look and feel styling for messages.

--- conversation.js
+++ conversation.js
@@ -17,11 +17,21 @@ $(document).ready(() => {
 ipc.on('receive_thread', (event, data)=>{
     thread = data.thread;
     userID = data.userID;
+    var isGroup = thread.participantIDs.length > 2;
 
-    $('#conversation-img').attr('src', thread.imageSrc);
-    $('#conversation_name-h1').text(thread.name);
+    var convImgSrc = (isGroup) ? thread.imageSrc : data.userInfo.thumbSrc;
+    var convName = (isGroup) ? thread.name : data.userInfo.name;
+
+    $('#conversation-img').attr('src', convImgSrc);
+    $('#conversation_name-h1').text(convName);
 });
 
 ipc.on('receive_history', (event, history)=>{
     event.sender.send('conversation_show');
 });
+
+
+
+function mainLog(log) {
+    ipc.send('console.log', log);
+}
