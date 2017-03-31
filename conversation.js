96f3fb85ea5b64ee0ae4480d700f1bf77be109a0
Kiefer Lam <mangopearandapples@gmail.com>
2017-03-31 13:57:10 +0100

    Added basic conversation loading.

--- conversation.js
+++ conversation.js
@@ -4,6 +4,10 @@ const remote = electron.remote;
 
 var ipc = electron.ipcRenderer;
 
+window.onerror = function (error, url, line) {
+    mainLog("Error at line " + line + ": " + error);
+};
+
 const $ = require('jquery');
 
 var thread = null;
@@ -18,6 +22,8 @@ $(document).ready(() => {
 ipc.on('receive_thread', (event, data)=>{
     thread = data.thread;
     userID = data.userID;
+    participantInfos = data.userInfos;
+
     var isGroup = thread.participantIDs.length > 2;
     var firstParticipant = data.userInfos[thread.participantIDs[0]];
 
@@ -28,7 +34,42 @@ ipc.on('receive_thread', (event, data)=>{
     $('#conversation_name-h1').text(convName);
 });
 
+function time_as_string(date){
+    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+}
+
+function makeTimestamp(name){
+    var date = new Date(Date.now());
+    var time = time_as_string(date);
+    return time + ' ' + name;
+}
+
+function getUserImg(userInfo){
+    return userInfo.thumbSrc;
+}
+
+function getMessageHTML(userInfo, messageBody){
+    var timestamp = makeTimestamp(userInfo.name);
+    var html = '<div class="conversation_message-div"> <div class="conversation_message_timestamp-div"> <label>'+timestamp+'</label></div><div class="conversation_message_sender_img-div"><img src="'+getUserImg(userInfo)+'" class="conversation_message_sender-img" alt="Sender Image" /></div><div class="conversation_message_content-div"><p>'+messageBody+'</p></div></div>';
+    return html;
+}
+
+function appendMessage(msg){
+    var userInfo = participantInfos[msg.senderID];
+    var html = getMessageHTML(userInfo, msg.body);
+    
+    $('#conversation_messages-div').append(html);
+}
+
 ipc.on('receive_history', (event, history)=>{
+
+    history.forEach((msg, index)=>{
+        messages.push(msg);
+        if(msg.type == 'message'){
+            appendMessage(msg);
+        }
+    });
+
     event.sender.send('conversation_show');
 });
 
